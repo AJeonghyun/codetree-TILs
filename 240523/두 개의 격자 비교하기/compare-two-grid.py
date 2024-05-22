d2e4f15391badb10f27a6b242314d3ba515fe5ef
AJeonghyun <112387593+AJeonghyun@users.noreply.github.com>
2024-05-23 00:55:10 +0900

    Title: 두 개의 격자 비교하기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240523/두 개의 격자 비교하기/compare-two-grid.py
@@ -0,0 +1,35 @@
+n,m = input().split()
+n = int(n)
+m = int(m)
+# 첫 번째 2차원 배열을 구현해 정수를 입력받습니다.
+arr_1 = [
+	list(map(int, input().split()))
+	for _ in range(4)
+]
+
+
+# 두 번째 2차원 배열을 구현해 정수를 입력받습니다.
+arr_2 = [
+	list(map(int, input().split()))
+	for _ in range(4)
+]
+
+# 2차원 배열을 구현합니다.
+arr_3 = [
+    [0 for _ in range(m)]
+    for _ in range(n)
+]
+
+# 두 배열의 곱을 새로운 배열에 담습니다.
+for i in range(n):
+    for j in range(m):
+        if arr_1[i][j] == arr_2[i][j]:
+            arr_3[i][j] = 0
+        else:
+            arr_3[i][j] = 1
+	
+# 새로운 배열을 출력합니다.
+for row in arr_3:
+	for elem in row:
+		print(elem, end=" ")
+	print()
