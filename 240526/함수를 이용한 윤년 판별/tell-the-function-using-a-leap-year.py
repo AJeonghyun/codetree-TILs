0d370f63c8a6545aaa3abba3e5a15d381ea9b13e
AJeonghyun <112387593+AJeonghyun@users.noreply.github.com>
2024-05-26 18:52:14 +0900

    Title: 함수를 이용한 윤년 판별, Time: 118ms, Memory: 26MB, Status: Passed - CodeTree

--- 240526/함수를 이용한 윤년 판별/tell-the-function-using-a-leap-year.py
+++ 240526/함수를 이용한 윤년 판별/tell-the-function-using-a-leap-year.py
@@ -1,8 +1,8 @@
 def abc(y):
+    if y%100==0 and y%400!=0:
+        return 'false'
     if y%4==0:
         return 'true'
-        if y%100==0 and y%400!=0:
-            return 'false'
     else:
         return 'false'
 
