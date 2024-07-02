1f69ee7bb5eed977d9fae9a41c9b70eba70e1396
AJeonghyun <112387593+AJeonghyun@users.noreply.github.com>
2024-07-03 00:17:35 +0900

    Title: 일렬로 서있는 소 2, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240703/일렬로 서있는 소 2/cattle-in-a-rowing-up-2.cpp
+++ 240703/일렬로 서있는 소 2/cattle-in-a-rowing-up-2.cpp
@@ -10,7 +10,7 @@ int main() {
     }
     for (int i=0; i<n; i++) {
         for (int j=i+1; j<n-1; j++) {
-            for (int k=i+2; k<n; k++) {
+            for (int k=j+1; k<n; k++) {
                 if ((i<j) && (j<k) && (i<k)) {
                     if((arr[i] < arr[j])&& (arr[j] < arr[k]) && (arr[i] < arr[k])) {
                         sum += 1;
