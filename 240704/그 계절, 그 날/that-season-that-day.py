4695665eb9fe20f4700aad457318b2536a44eaad
AJeonghyun <112387593+AJeonghyun@users.noreply.github.com>
2024-07-04 21:47:01 +0900

    Title: 그 계절, 그 날, Time: 103ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240704/그 계절, 그 날/that-season-that-day.py
+++ 240704/그 계절, 그 날/that-season-that-day.py
@@ -13,27 +13,51 @@ def day_test(year,month,day):
         yoon = 0
     
     if(yoon==1):
-        if(1<=year<=3000) and (1<=month<=12) and (1<=day<=29):
+        if(1<=year<=3000) and (1<=month<=12) and (1<=day<=31):
             if(3<=month<=5):
-                print("Spring")
+                if(month==4 and day==31):
+                    print(-1)
+                else:
+                    print("Spring")
             elif (6<=month<=8):
-                print("Summer")
+                if(month==6 and day==31):
+                    print(-1)
+                else:
+                    print("Summer")
             elif (9<=month<=11):
-                print("Fall")
+                if(month==9 or month==11 and day==31):
+                    print(-1)
+                else:
+                    print("Fall")
             else:
-                print("Winter")
+                if(month==2 and day>29):
+                    print(-1)
+                else:
+                    print("Winter")
         else:
             print(-1)
     else:
         if(1<=year<=3000) and (1<=month<=12) and (1<=day<=31):
             if(3<=month<=5):
-                print("Spring")
+                if(month==4 and day==31):
+                    print(-1)
+                else:
+                    print("Spring")
             elif (6<=month<=8):
-                print("Summer")
+                if(month==6 and day==31):
+                    print(-1)
+                else:
+                    print("Summer")
             elif (9<=month<=11):
-                print("Fall")
+                if(month==9 or month==11 and day==31):
+                    print(-1)
+                else:
+                    print("Fall")
             else:
-                print("Winter")
+                if(month==2 and day>28):
+                    print(-1)
+                else:
+                    print("Winter")
         else:
             print(-1)
 
