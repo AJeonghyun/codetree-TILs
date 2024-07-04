7e607c33bdd1a212f851997ebbc23e2a7230221a
AJeonghyun <112387593+AJeonghyun@users.noreply.github.com>
2024-07-04 21:16:22 +0900

    Title: 그 계절, 그 날, Time: 107ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240704/그 계절, 그 날/that-season-that-day.py
+++ 240704/그 계절, 그 날/that-season-that-day.py
@@ -10,14 +10,14 @@ if (1<=year<=3000) and (1<=month<=12):
         if(year%100==0 and year%400==0):
             if(30<=day<=31):
                 print(-1)
-
-        if(3<=month<=5):
-            print("Spring")
-        elif (6<=month<=8):
-            print("Summer")
-        elif (9<=month<=11):
-            print("Fall")
         else:
-            print("Winter")
+            if(3<=month<=5):
+                print("Spring")
+            elif (6<=month<=8):
+                print("Summer")
+            elif (9<=month<=11):
+                print("Fall")
+            else:
+                print("Winter")
 else :
     print(-1)
