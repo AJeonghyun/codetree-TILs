622f6a88b75a57292e459c7350c413c8721bddc8
AJeonghyun <112387593+AJeonghyun@users.noreply.github.com>
2024-07-04 21:25:21 +0900

    Title: 그 계절, 그 날, Time: 118ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240704/그 계절, 그 날/that-season-that-day.py
+++ 240704/그 계절, 그 날/that-season-that-day.py
@@ -5,12 +5,9 @@ day = int(day)
 
 
 
-if (1<=year<=3000) and (1<=month<=12):
-    if(year%4==0):
-        if(year%100==0 and year%400==0):
-            if(30<=day<=31):
-                print(-1)
-        else:
+if(year%4==0):
+    if(year%100==0):
+        if (1<=year<=3000) and (1<=month<=12) and (1<=day<=31):
             if(3<=month<=5):
                 print("Spring")
             elif (6<=month<=8):
@@ -19,5 +16,40 @@ if (1<=year<=3000) and (1<=month<=12):
                 print("Fall")
             else:
                 print("Winter")
-else :
-    print(-1)
+        else:
+            print(-1)
+
+        if(year%400==0):
+            if (1<=year<=3000) and (1<=month<=12) and (1<=day<=29):
+                if(3<=month<=5):
+                    print("Spring")
+                elif (6<=month<=8):
+                    print("Summer")
+                elif (9<=month<=11):
+                    print("Fall")
+                else:
+                    print("Winter")
+            else:
+                print(-1)
+    else:
+        if (1<=year<=3000) and (1<=month<=12) and (1<=day<=31):
+            if(3<=month<=5):
+                print("Spring")
+            elif (6<=month<=8):
+                print("Summer")
+            elif (9<=month<=11):
+                print("Fall")
+            else:
+                print("Winter")
+        else:
+            print(-1)
+else:
+    if (1<=year<=3000) and (1<=month<=12) and (1<=day<=31):
+        if(3<=month<=5):
+            print("Spring")
+        elif (6<=month<=8):
+            print("Summer")
+        elif (9<=month<=11):
+            print("Fall")
+        else:
+            print("Winter")
