90e0a9ac593829321fa32592ae57a3ceb16d38b0
AJeonghyun <112387593+AJeonghyun@users.noreply.github.com>
2024-07-04 21:34:32 +0900

    Title: 그 계절, 그 날, Time: 98ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240704/그 계절, 그 날/that-season-that-day.py
+++ 240704/그 계절, 그 날/that-season-that-day.py
@@ -1,44 +1,14 @@
 def day_test(year,month,day):
     if(year%4==0):
+        yoon = 1
         if(year%100==0):
-            if (1<=year<=3000) and (1<=month<=12) and (1<=day<=31):
-                if(3<=month<=5):
-                    print("Spring")
-                elif (6<=month<=8):
-                    print("Summer")
-                elif (9<=month<=11):
-                    print("Fall")
-                else:
-                    print("Winter")
-            else:
-                print(-1)
-
+            yoon = 0
             if(year%400==0):
-                if (1<=year<=3000) and (1<=month<=12) and (1<=day<=29):
-                    if(3<=month<=5):
-                        print("Spring")
-                    elif (6<=month<=8):
-                        print("Summer")
-                    elif (9<=month<=11):
-                        print("Fall")
-                    else:
-                        print("Winter")
-                else:
-                    print(-1)
-        else:
-            if (1<=year<=3000) and (1<=month<=12) and (1<=day<=31):
-                if(3<=month<=5):
-                    print("Spring")
-                elif (6<=month<=8):
-                    print("Summer")
-                elif (9<=month<=11):
-                    print("Fall")
-                else:
-                    print("Winter")
-            else:
-                print(-1)
+                yoon = 1
     else:
-        if (1<=year<=3000) and (1<=month<=12) and (1<=day<=31):
+        yoon = 0
+    if(yoon==1):
+        if(1<=year<=3000) and (1<=month<=12) and (1<=day<=29):
             if(3<=month<=5):
                 print("Spring")
             elif (6<=month<=8):
@@ -49,7 +19,18 @@ def day_test(year,month,day):
                 print("Winter")
         else:
             print(-1)
-
+    else:
+        if(1<=year<=3000) and (1<=month<=12) and (1<=day<=29):
+            if(3<=month<=5):
+                print("Spring")
+            elif (6<=month<=8):
+                print("Summer")
+            elif (9<=month<=11):
+                print("Fall")
+            else:
+                print("Winter")
+        else:
+            print(-1)
 
 year,month,day = input().split(" ")
 year = int(year)
